5ee3230b6a5f088073f54031e9c14c4911399c63
Matt W. <human@mattgonewild.com>
2025-09-08 00:19:15 -0500

    feat: add Pack16 and Unpack16

---
+++ help.go
@@ -0,0 +1,23 @@
+package kit
+
+import (
+	"encoding/binary"
+	"time"
+
+	"github.com/mattgonewild/kit/internal/help"
+)
+
+func Pack16(low, high uint64) (array [help.SixteenInt]byte) {
+	binary.LittleEndian.PutUint64(array[help.ZeroInt:help.EightInt], low)
+	binary.LittleEndian.PutUint64(array[help.EightInt:help.SixteenInt], high)
+	return array
+}
+
+func Unpack16(array [help.SixteenInt]byte) (low, high uint64) {
+	low = binary.LittleEndian.Uint64(array[help.ZeroInt:help.EightInt])
+	high = binary.LittleEndian.Uint64(array[help.EightInt:help.SixteenInt])
+	return low, high
+}
+
+func UnixNano() int64         { return time.Now().UnixNano() }
+func BoolToInt(true bool) int { return help.BoolToInt(true) }
